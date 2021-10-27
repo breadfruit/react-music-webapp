1c9affe17d82642b37c157f68dabb4ddf5b49bd2
wengjiaqi <1845252982@qq.com>
2021-10-28 00:05:20 +0800

    feat: 基本骨架

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { GlobalStyle } from  './style';
+import { IconStyle } from './assets/iconfont/iconfont'
+import { HashRouter } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
+import route from './route/index'
+import store from './store/index'
+function App() {
+  return (
+    <HashRouter store={store}>
+      <GlobalStyle></GlobalStyle>
+      <IconStyle></IconStyle>
+      <i className="iconfont">&#xe62b;</i>
+      {renderRoutes(route)}
+    </HashRouter>
+  );
+}
+
+export default App;
