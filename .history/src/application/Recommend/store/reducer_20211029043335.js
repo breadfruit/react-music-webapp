cee17964414946bf84459d0c8f3e9d4e994c6f7d
wengjiaqi <1845252982@qq.com>
2021-10-31 11:37:47 +0800

    fix_meishi_1031
    fix: package

--- .history/src/application/Recommend/store/reducer_20211029043335.js
+++
@@ -1,2 +0,0 @@
-mport * as actionTypes from './constants';
-import { fromJS } from 'immutable';// 这里用到 fromJS 把 JS 数据结构转化成 immutable 数据结构
--- .history/src/store/reducer_20211028215230.js
+++
@@ -1,8 +0,0 @@
-//reducer.js
-import * as actionTypes from './constants';
-import { fromJS } from 'immutable';// 这里用到 fromJS 把 JS 数据结构转化成 immutable 数据结构
-
-const defaultState = fromJS ({
-  bannerList: [],
-  recommendList: [],
-});
