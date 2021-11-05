e8625f009d2c489114665e79cc8aa1865b56b549
美式因子 <q1845252982@163.com>
2021-11-05 17:00:32 +0800

    进度条拖动

--- src/baseUI/progressBar/index.js
+++ src/baseUI/progressBar/index.js
@@ -1,48 +1,120 @@
-//baseUI/progressBar/index.js
 import React, {useEffect, useRef, useState } from 'react';
-import styled from'styled-components';
+import styled from 'styled-components';
 import style from '../../assets/global-style';
 import { prefixStyle } from './../../api/utils';
 
 const ProgressBarWrapper = styled.div`
   height: 30px;
-  .bar-inner {
+  .bar-inner{
     position: relative;
     top: 13px;
     height: 4px;
-    background: rgba (0, 0, 0, .3);
-    .progress {
+    background: rgba(0, 0, 0, .3);
+    .progress{
       position: absolute;
       height: 100%;
-      background: ${style ["theme-color"]};
+      background: ${style["theme-color"]};
     }
-    .progress-btn-wrapper {
+    .progress-btn-wrapper{
       position: absolute;
-      left: -15px;
+      left: -8px;
       top: -13px;
       width: 30px;
       height: 30px;
-      .progress-btn {
+      .progress-btn{
         position: relative;
         top: 7px;
         left: 7px;
         box-sizing: border-box;
         width: 16px;
         height: 16px;
-        border: 3px solid ${style ["border-color"]};
+        border: 3px solid ${style["border-color"]};
         border-radius: 50%;
-        background: ${style ["theme-color"]};
+        background: ${style["theme-color"]};
       }
     }
   }
 `
 
-function ProgressBar (props) {
+function ProgressBar(props){
+  const progressBar = useRef();
+  const progress = useRef();
+  const progressBtn = useRef();
+  const [touch, setTouch] = useState({});
+
+  const { percent } = props;
+
+  const transform = prefixStyle('transform');
+
+  const progressBtnWidth = 16;  
+
+  useEffect(() => {
+    if(percent >= 0 && percent <= 1 && !touch.initiated) {
+      const barWidth = progressBar.current.clientWidth - progressBtnWidth;
+      const offsetWidth = percent * barWidth;
+      progress.current.style.width = `${offsetWidth}px`;
+      progressBtn.current.style[transform] = `translate3d(${offsetWidth}px, 0, 0)`;
+    }
+    // eslint-disable-next-line
+  }, [percent])
+
+  const _offset = (offsetWidth) => {
+    progress.current.style.width = `${offsetWidth}px`;
+    progressBtn.current.style.transform = `translate3d(${offsetWidth}px, 0, 0)`;
+  }
+
+//   const _changePercent = () => {
+//     const barWidth = progressBar.current.clientWidth - progressBtnWidth;
+//     const curPercent = progress.current.clientWidth / barWidth;
+//     props.percentChange(curPercent);
+//   }
+
+  const progressClick = (e) => {
+    console.log('e', e)
+    const rect = progressBar.current.getBoundingClientRect();
+    console.log('rect', rect)
+    const offsetWidth = e.pageX - rect.left;
+    _offset(offsetWidth);
+    // _changePercent(percent);
+  }
+
+  const progressTouchStart = (e) => {
+    console.log('progressTouchStart', e)
+    console.log('progress.current.clientWidth----', progress.current.clientWidth)
+    const startTouch = {};
+    startTouch.initiated = true;
+    startTouch.startX = e.touches[0].pageX;
+    startTouch.left = progress.current.clientWidth;
+    setTouch(startTouch);
+  }
+
+  const progressTouchMove = (e) => {
+      // pageX 是一个由MouseEvent接口返回的相对于整个文档的x（水平）坐标以像素为单位的只读属性
+    console.log('progressTouchMove', e)
+    if(!touch.initiated) return;
+    const deltaX = e.touches[0].pageX - touch.startX;
+    const barWidth = progressBar.current.clientWidth - progressBtnWidth; 
+    const offsetWidth = Math.min(Math.max(0, touch.left + deltaX), barWidth);
+    _offset(offsetWidth);
+  }
+
+  const progressTouchEnd = (e) => {
+    console.log('progressTouchEnd', e)
+    const endTouch = JSON.parse(JSON.stringify(touch));
+    endTouch.initiated = false;
+    setTouch(endTouch);
+    // _changePercent();
+  }
+
   return (
     <ProgressBarWrapper>
-      <div className="bar-inner">
-        <div className="progress"></div>
-        <div className="progress-btn-wrapper">
+      <div className="bar-inner" ref={progressBar} onClick={progressClick}>
+        <div className="progress" ref={progress}></div>
+        <div className="progress-btn-wrapper" ref={progressBtn}
+            onTouchStart={progressTouchStart}
+            onTouchMove={progressTouchMove}
+            onTouchEnd={progressTouchEnd}
+        >
           <div className="progress-btn"></div>
         </div>
       </div>
@@ -50,4 +122,4 @@ function ProgressBar (props) {
   )
 }
 
-export default React.memo(ProgressBar)
+export default React.memo(ProgressBar);
